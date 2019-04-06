83f377d21f4085b2fd9c380555181d01740cac71
Alexey Frolov <gartenzius@gmail.com>
2019-04-06 21:02:29 +0300

    Add tests for delete method of Answer

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -60,4 +60,17 @@ RSpec.describe AnswersController, type: :controller do
       end
     end
   end
+
+  describe 'DELETE #destroy' do
+    let!(:answer) { create(:answer, :with_question) }
+
+    it 'deletes question' do
+      expect { delete :destroy, params: { id: answer } }.to change(Answer, :count).by(-1)
+    end
+
+    it 'redirects to index view' do
+      delete :destroy, params: { id: answer }
+      expect(response).to redirect_to answer.question
+    end
+  end
 end
