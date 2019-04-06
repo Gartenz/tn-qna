8923a06d3fed5e56826b16561a660740203fc80c
Alexey Frolov <gartenzius@gmail.com>
2019-04-06 16:13:24 +0300

    add tests for new and edit method of Question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,23 @@
 class QuestionsController < ApplicationController
+  before_action :load_questions, only: %i[show edit]
+
   def index
     @questions = Question.all
   end
 
   def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def load_questions
     @question = Question.find(params[:id])
   end
 end
