3eab5dbef85ff0f47c3d283c4391c36e6047e24b
Alexey Frolov <gartenzius@gmail.com>
2019-04-08 19:52:25 +0300

    Add acceptance tests for 'user can see list of questions' history

---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'List questions', %q{
+  if users want to create non duplicant question
+} do
+  describe 'Authenticated user' do
+    given!(:questions) { create_list(:question, 3) }
+    given(:user) { create(:user) }
+
+    background { sign_in(user) }
+
+    scenario 'wants to see list of questions' do
+      visit questions_path
+
+      expect(page).to have_content questions[0].title
+    end
+
+  end
+
+  scenario 'Unauthenticated user wants to see list of questions' do
+    visit questions_path
+
+    expect(page).to have_content questions[0].title
+  end
+end
