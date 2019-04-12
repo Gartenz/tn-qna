a44f88904347f58c20e9628c2dfde858dbe0a1c6
Alexey Frolov <gartenzius@gmail.com>
2019-04-13 00:12:29 +0300

    Add 'User can add file to question'

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -47,4 +47,8 @@ RSpec.describe Question, type: :model do
       expect(old_best.best).to eq true
     end
   end
+
+  it 'have one attached file' do
+    expect(Question.new.file).to be_an_instance_of(ActiveStorage::Attached::One) 
+  end
 end
