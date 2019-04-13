06d2f07f6f165b4ec8858432a6e24fe7f2177628
Alexey Frolov <gartenzius@gmail.com>
2019-04-13 14:23:50 +0300

    Add fixes after code review

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Question, type: :model do
   describe '#best_answer returns' do
     let!(:user) { create(:user) }
     let!(:question) { create(:question, user: user) }
-    let!(:answer) { create(:answer, question: question, best: true)}
+    let!(:answer) { create(:answer, question: question, best: true) }
 
     it 'answer if question have best anwer' do
       expect(question.best_answer).to eq answer
@@ -19,6 +19,32 @@ RSpec.describe Question, type: :model do
     it 'nil if question does not have best answer' do
       question = create(:question, user: user)
       expect(question.best_answer).to eq nil
+    end
+  end
+
+  describe '#mark_best' do
+    let!(:user) { create(:user) }
+    let!(:question) { create(:question, user: user) }
+    let!(:old_best) { create(:answer, question: question, best: true) }
+
+    it 'updates best answer of question with new  best answer' do
+      new_best = create(:answer, question: question)
+      question.mark_best(new_best)
+
+      old_best.reload
+      new_best.reload
+
+      expect(old_best.best).to eq false
+      expect(new_best.best).to eq true
+    end
+
+    it 'did not updates if answer do not belong to question' do
+      answer = create(:answer)
+      question.mark_best(answer)
+
+      old_best.reload
+
+      expect(old_best.best).to eq true
     end
   end
 end
