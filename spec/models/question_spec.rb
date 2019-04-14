3674d9245904552ead7b4dbcdbc5461c634e902a
Alexey Frolov <gartenzius@gmail.com>
2019-04-14 15:06:27 +0300

    add fixes after rebase with master branch

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -49,6 +49,6 @@ RSpec.describe Question, type: :model do
   end
 
   it 'have one attached file' do
-    expect(Question.new.file).to be_an_instance_of(ActiveStorage::Attached::One) 
+    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many) 
   end
 end
