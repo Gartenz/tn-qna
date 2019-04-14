ec3f1fc8ac175e0010aac8d8c91429d519e57bcd
Alexey Frolov <gartenzius@gmail.com>
2019-04-14 19:17:27 +0300

    Add 'User can delete file of his answers'

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -8,6 +8,10 @@ FactoryBot.define do
     user
     question
 
+    trait :with_file do
+      files { fixture_file_upload(Rails.root.join('spec','rails_helper.rb')) }
+    end
+
     trait :invalid do
       body { nil }
     end
