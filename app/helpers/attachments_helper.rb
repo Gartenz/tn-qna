b2407f92fda28d874cb9f8dff38e6078cfa3b3a3
Alexey Frolov <gartenzius@gmail.com>
2019-04-15 12:19:34 +0300

    Add fix

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,9 +1,9 @@
 module AttachmentsHelper
-  def is_record_question?(record)
+  def is_question?(record)
     record.is_a?(Question)
   end
 
-  def is_record_answer?(record)
+  def is_answer?(record)
     record.is_a?(Answer)
   end
 end
