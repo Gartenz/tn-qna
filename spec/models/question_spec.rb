3383d015c3d37c1ea198e1f14adc955208c0b5a0
Alexey Frolov <gartenzius@gmail.com>
2019-04-15 22:52:30 +0300

    Add 'User can add one link to question'

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:links).dependent(:destroy) }
   it { should belong_to(:user) }
+
+  it { should accept_nested_attributes_for :links }
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
@@ -49,6 +52,6 @@ RSpec.describe Question, type: :model do
   end
 
   it 'have one attached file' do
-    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many) 
+    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
 end
