9a9d96565d0271fb19dac4e01eeb1a2d0a525a77
Alexey Frolov <gartenzius@gmail.com>
2019-04-16 12:07:14 +0300

    Add url validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,20 @@ class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
+  validate :validates_url_format
+
+  private
+
+  def validates_url_format
+    unless url.present? && compliant_url?
+      self.errors.add(:url, "is not a valid HTTP URL")
+    end
+  end
+
+  def compliant_url?
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) && !uri.host.nil?
+  rescue URI::InvalidURIError
+    false
+  end
 end
