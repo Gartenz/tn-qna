b9892421269bdd2410473b9fbed90305475ed7a5
Alexey Frolov <gartenzius@gmail.com>
2019-04-17 15:20:49 +0300

    Add forgotten shoving gist for question

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -7,17 +7,36 @@ feature 'User can add links ti question', %q{
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/Gartenz/2b300505c12cf48e38c95d00edb7fd0b' }
 
-  scenario 'User adds link when asks question' do
-    sign_in user
-    visit new_question_path
+  describe 'Authenticated user' do
+    background do
+      sign_in user
+      visit new_question_path
+    end
 
-    fill_in 'Title', with: 'Test title'
-    fill_in 'Body', with: 'Test text question'
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    scenario 'User adds link when asks question' do
 
-    click_on 'Create Question'
+      fill_in 'Title', with: 'Test title'
+      fill_in 'Body', with: 'Test text question'
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
 
-    expect(page).to have_link 'My gist', href: gist_url
+      click_on 'Create Question'
+
+      expect(page).to have_link 'My gist', href: gist_url
+    end
+
+    scenario 'adds gist link when give an answer' do
+      fill_in 'Title', with: 'Test title'
+      fill_in 'Body', with: 'Test text question'
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
+
+      click_on 'Create Question'
+
+      within '.question-body' do
+        expect(page).to have_link 'My gist', href: gist_url
+        expect(page).to have_content 'My gist content'
+      end
+    end
   end
 end
