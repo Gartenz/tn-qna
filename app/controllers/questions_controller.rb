1dceb143ba82d42c746895b031601e78322065da
Alexey Frolov <gartenzius@gmail.com>
2019-04-17 15:16:30 +0300

    Add 'User can edit question for add or remove links'

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -39,7 +39,7 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body,
-                                     files: [], links_attributes: [:name, :url, :_destroy])
+                                     files: [], links_attributes: [:id, :name, :url, :_destroy])
   end
 
 end
