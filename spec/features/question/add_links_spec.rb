f37675ac43b662ef427c473b94cb053e38dde67e
Alexey Frolov <gartenzius@gmail.com>
2019-04-18 20:53:52 +0300

    Add tests fixes for multiple links

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -5,6 +5,7 @@ feature 'User can add links ti question', %q{
   I'd like to be able to add links
 } do
   given(:user) { create(:user) }
+  given(:simple_urls) { ['https://google.com', 'https://yandex.ru'] }
   given(:gist_url) { 'https://gist.github.com/Gartenz/2b300505c12cf48e38c95d00edb7fd0b' }
 
   describe 'Authenticated user' do
@@ -13,17 +14,28 @@ feature 'User can add links ti question', %q{
       visit new_question_path
     end
 
-    scenario 'User adds link when asks question' do
+    scenario 'User adds link when asks question', js: true do
       within '.question' do
         fill_in 'Title', with: 'Test title'
         fill_in 'Body', with: 'Test text question'
       end
-      fill_in 'Link name', with: 'My gist'
-      fill_in 'Url', with: gist_url
 
-        click_on 'Create Question'
+      click_on 'add link'
 
-      expect(page).to have_link 'My gist', href: gist_url
+      nested_fields = all('div.nested-fields')
+
+      nested_fields.each.with_index do |group, index|
+        within group do
+          fill_in 'Link name', with: 'My link'
+          fill_in 'Url', with: simple_urls[index]
+        end
+      end
+
+      click_on 'Create Question'
+
+      within '.card-body' do
+        simple_urls.each { |url| expect(page).to have_link 'My link', href: url }
+      end
     end
 
     scenario 'adds gist link when give an answer' do
