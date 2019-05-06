aaa304b99cd64a03f923652cb0253f0ae27c3ed9
Alexey Frolov <gartenzius@gmail.com>
2019-05-06 23:41:57 +0300

    Add tests for questions

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -7,50 +7,84 @@ feature 'User can create question', %q{
 } do
   given(:user) { create(:user) }
 
-  describe 'Authenticated user' do
-    background do
-      sign_in(user)
+  describe 'Using one session' do
+    context 'Authenticated user' do
+      background do
+        sign_in(user)
+        visit questions_path
+        click_on 'Ask question'
+      end
+
+      scenario 'asks a question' do
+        within '.question' do
+          fill_in 'Title', with: 'Test question'
+          fill_in 'Body', with: 'text text'
+        end
+
+        click_on 'Create Question'
+
+        expect(page).to have_content 'Your question successfully created'
+        expect(page).to have_content 'Test question'
+        expect(page).to have_content 'text text'
+      end
+
+      scenario 'asks a question with errors' do
+        click_on 'Create Question'
+
+        expect(page).to have_content "Title can't be blank"
+      end
+
+      scenario 'asks question with attached file' do
+        within '.question' do
+          fill_in 'Title', with: 'Test question'
+          fill_in 'Body', with: 'text text'
+        end
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Create Question'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
+
+    scenario 'Unauthenticated user tries to ask a question' do
       visit questions_path
       click_on 'Ask question'
-    end
 
-    scenario 'asks a question' do
-      within '.question' do
-        fill_in 'Title', with: 'Test question'
-        fill_in 'Body', with: 'text text'
-      end
-
-      click_on 'Create Question'
-
-      expect(page).to have_content 'Your question successfully created'
-      expect(page).to have_content 'Test question'
-      expect(page).to have_content 'text text'
-    end
-
-    scenario 'asks a question with errors' do
-      click_on 'Create Question'
-
-      expect(page).to have_content "Title can't be blank"
-    end
-
-    scenario 'asks question with attached file' do
-      within '.question' do
-        fill_in 'Title', with: 'Test question'
-        fill_in 'Body', with: 'text text'
-      end
-
-      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
-      click_on 'Create Question'
-
-      expect(page).to have_link 'rails_helper.rb'
-      expect(page).to have_link 'spec_helper.rb'
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
     end
   end
 
-  scenario 'Unauthenticated user tries to ask a question' do
-    visit questions_path
-    click_on 'Ask question'
+  describe 'Using multiple sessions', js: true do
+    scenario 'Question appears on another browser' do
+      Capybara.using_session('user') do
+        sign_in user
+        visit questions_path
+        click_on 'Ask question'
+      end
 
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+      Capybara.using_session('guest') do
+        visit questions_path
+      end
+
+      Capybara.using_session('user') do
+        within '.question' do
+          fill_in 'Title', with: 'Test question'
+          fill_in 'Body', with: 'text text'
+        end
+
+        click_on 'Create Question'
+
+        expect(page).to have_content 'Your question successfully created'
+        expect(page).to have_content 'Test question'
+        expect(page).to have_content 'text text'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to have_content 'Test question'
+        expect(page).to have_content 'text text'
+      end
+    end
   end
 end
