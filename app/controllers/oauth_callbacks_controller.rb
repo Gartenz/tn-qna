93b47224103622540e5259a8b05c2c4576684116
Alexey Frolov <gartenzius@gmail.com>
2019-05-15 18:47:07 +0300

    Add vkontakte omniauth without confirmation

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -11,10 +11,20 @@ class OauthCallbacksController < Devise::OmniauthCallbacksController
 
   def vkontakte
     auth = request.env['omniauth.auth']
-    email =auth.info.email
+    email = auth.info.email
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    if authorization
+      sign_in_and_redirect authorization.user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Vkontakte') if is_navigational_format?
+      return
+    end
+
     if email.nil?
       password = Devise.friendly_token[0, 20]
       user = User.create(email: "changeme.#{auth.credentials.expires_at}@email.com", password: password, password_confirmation: password)
+      user.create_authorization(auth)
+      sign_in user
+      redirect_to add_email_signup_path(user)
     end
   end
 end
