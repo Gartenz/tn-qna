9d0bfb91565f1d0876b17f7fcf0341dafbcad7ec
Alexey Frolov <gartenzius@gmail.com>
2019-05-22 18:14:53 +0300

    After 'serializers' screencast

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
       resources :profiles, only: [] do
         get :me, on: :collection
       end
+
+      resources :questions, only: %i[index]
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_142402) do
+ActiveRecord::Schema.define(version: 2019_05_22_145038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_05_21_142402) do
     t.bigint "question_id"
     t.bigint "user_id"
     t.boolean "best", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
