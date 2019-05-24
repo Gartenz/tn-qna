adda88f4aea5b716880681a8712a04f83c1b62e4
Alexey Frolov <gartenzius@gmail.com>
2019-05-24 23:55:54 +0300

    Add abilities to API

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,12 +1,17 @@
 class Api::V1::BaseController < ApplicationController
-  skip_authorization_check
-  skip_authorize_resource
-
   before_action :doorkeeper_authorize!
+
+  rescue_from CanCan::AccessDenied do |exception|
+    head :unauthorized
+  end
 
   private
 
   def current_resource_onwer
     @current_resource_onwer ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_resource_onwer)
   end
 end
