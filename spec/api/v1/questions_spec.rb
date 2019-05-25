b094a6a30ba48256abc731692e0a90c413066091
Alexey Frolov <gartenzius@gmail.com>
2019-05-25 13:31:50 +0300

    Refactor some code

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -3,39 +3,30 @@ require 'rails_helper'
 describe 'Questions API', type: :request do
   let(:headers) { { 'CONTENT_TYPE' => 'application/json',
                     'ACCEPT' => 'application/json' } }
+  let(:access_token) { create(:access_token) }
 
   describe 'GET /api/v1/questions' do
     let(:api_path) { '/api/v1/questions' }
 
     it_behaves_like 'API Authorizable' do
       let(:method) { :get }
+      let(:params) { {access_token: access_token.token} }
     end
 
     context 'authorized' do
-      let(:access_token) { create(:access_token) }
       let!(:questions) { create_list(:question, 2) }
       let(:question) { questions.first }
-      let(:question_response) { json['questions'].first }
-      let!(:answers) { create_list(:answer, 3, question: question) }
+      let(:list_response) { json['questions'] }
 
       before { get api_path, params: { access_token: access_token.token }, headers: headers }
 
-      it 'reuturns 200 status' do
-        expect(response).to be_successful
-      end
-
-      it 'return list of questions' do
-        expect(json['questions'].size).to eq 2
-      end
-
-      it 'returns all public fields' do
-        %w[id title body created_at updated_at].each do |attr|
-          expect(question_response[attr]).to eq question.send(attr).as_json
-        end
+      it_behaves_like 'API Listable' do
+        let(:list) { questions }
+        let(:fields) { %w[id title body created_at updated_at] }
       end
 
       it 'contains user object' do
-        expect(question_response['user']['id']).to eq question.user.id
+        expect(list_response.first['user']['id']).to eq question.user.id
       end
     end
   end
@@ -47,11 +38,10 @@ describe 'Questions API', type: :request do
 
     it_behaves_like 'API Authorizable' do
       let(:method) { :get }
+      let(:params) { { access_token: access_token.token } }
     end
 
     context 'authorized' do
-      let(:user) { create(:user) }
-      let(:access_token) { create(:access_token) }
       let(:question_response) { json['question'] }
 
       it 'returns nil if question does not exists' do
@@ -61,10 +51,6 @@ describe 'Questions API', type: :request do
 
       context 'question exists' do
         before { get api_path, params: { id: question.id, access_token: access_token.token }, headers: headers }
-
-        it 'reuturns 200 status' do
-          expect(response).to be_successful
-        end
 
         it 'returns public fields' do
           %w[id title body created_at updated_at].each do |attr|
@@ -77,17 +63,10 @@ describe 'Questions API', type: :request do
         end
 
         context 'links' do
-          let(:link_response) { question_response['links'].first }
-          let(:link) { question.links.first }
-
-          it 'returns list of links' do
-            expect(question_response['links'].size).to eq 2
-          end
-
-          it 'returns all public fields' do
-            %w[id name url].each do |attr|
-              expect(link_response[attr]).to eq link.send(attr).as_json
-            end
+          it_behaves_like 'API Listable' do
+            let(:list_response) { question_response['links'] }
+            let(:list) { question.links }
+            let(:fields) { %w[id name url] }
           end
         end
 
@@ -110,79 +89,63 @@ describe 'Questions API', type: :request do
           let(:comment_response) { question_response['comments'].first }
           let(:comment) { question.comments.first }
 
-          it 'returns list of comments' do
-            expect(question_response['comments'].size).to eq 2
-          end
-
-          it 'returns all public fields' do
-            %w[id body user_id].each do |attr|
-              expect(comment_response[attr]).to eq comment.send(attr).as_json
-            end
+          it_behaves_like 'API Listable' do
+            let(:list_response) { question_response['comments'] }
+            let(:list) { question.comments }
+            let(:fields) { %w[id body user_id] }
           end
         end
       end
     end
   end
 
-  # Создания, редактирования и удаления вопроса (без возможности прикреплять файлы)
   describe 'POST /api/v1/questions' do
     let(:api_path) { "/api/v1/questions" }
 
     it_behaves_like 'API Authorizable' do
       let(:method) { :post }
+      let(:params) { { question: attributes_for(:question), access_token: access_token.token } }
       let(:headers) { nil }
     end
 
     context 'authorized' do
-      let(:access_token) { create(:access_token) }
-
-      it 'reuturns 200 status' do
-        post api_path, params: { question: attributes_for(:question), access_token: access_token.token }
-        expect(response).to be_successful
-      end
-
       it 'creates new question' do
         expect { post api_path, params: { question: attributes_for(:question), access_token: access_token.token }}.to change(Question, :count).by(1)
       end
 
       it 'return Bad Request if no question params sended' do
         post api_path, params: { access_token: access_token.token }
+
         expect(response.status).to eq 400
       end
 
       it 'return Bad Request if bad question params sended' do
         bad_params = { 'titdle': '12312', 'bodt': '123123' }
-
         post api_path, params: { question: bad_params, access_token: access_token.token }
+
         expect(response.status).to eq 400
       end
     end
   end
 
   describe 'PATCH /api/v1/questions/:id' do
-    let!(:user) { create(:user) }
     let!(:question) { create(:question) }
     let(:api_path) { "/api/v1/questions/#{question.id}" }
 
     it_behaves_like 'API Authorizable' do
       let(:method) { :patch }
+      let(:params) { { question: attributes_for(:question), access_token: access_token.token } }
       let(:headers) { nil }
     end
 
     context 'authorized' do
-      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
-
-      it 'reuturns 200 status' do
-        patch api_path, params: { question: attributes_for(:question), access_token: access_token.token }
-        expect(response).to be_successful
-      end
-
       it 'creates new question' do
         new_params = {
           title: "New Title",
         }
         patch api_path, params: { question: new_params, access_token: access_token.token }
         question.reload
+
         expect(question.title).to eq new_params[:title]
       end
 
@@ -193,41 +156,32 @@ describe 'Questions API', type: :request do
 
       it 'return Bad Request if bad question params sended' do
         bad_params = { 'titdle': '12312', 'bodt': '123123' }
-
         patch api_path, params: { question: bad_params, access_token: access_token.token }
 
-        question.reload
         expect(question.title).to_not eq bad_params[:titdle]
       end
     end
   end
 
   describe 'DELETE /api/v1/questions/:id' do
-    let!(:user) { create(:user) }
     let!(:question) { create(:question) }
     let(:api_path) { "/api/v1/questions/#{question.id}" }
 
     it_behaves_like 'API Authorizable' do
       let(:method) { :delete }
+      let(:params) { { access_token: access_token.token } }
       let(:headers) { nil }
     end
 
     context 'authorized' do
-      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
-
-      it 'reuturns 200 status' do
-        delete api_path, params: { access_token: access_token.token }
-        expect(response).to be_successful
-      end
-
       it 'deletes question' do
         expect { delete api_path, params: { access_token: access_token.token } }.to change(Question, :count).by(-1)
       end
 
       it 'return Bad Request if no question found' do
         bad_path = "/api/v1/questions/-1"
-
         delete bad_path, params: { access_token: access_token.token }
+
         expect(response.status).to eq 400
       end
     end
