ad567e6a80b09fe95f89e57bc816f5d79d9f9d75
Alexey Frolov <gartenzius@gmail.com>
2019-06-07 14:31:21 +0300

    Add cache for question collection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,11 @@ module ApplicationHelper
   def comment?(record)
     record.is_a?(Comment)
   end
+
+  def collection_cache_for(model)
+    klass = model.to_s.capitalize.constantize
+    count = klass.count
+    max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s)
+    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
+  end
 end
